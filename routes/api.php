601a3c638fa0b4f0090be5cf9e870bbac6bac0bd
fule <fule@localhost.localdomain>
2020-01-10 09:43:40 +0800

    登录

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("/reg","UserController@reg");//注册
+Route::post("/login","UserController@login");//登录
+Route::get("/list","UserController@list")->middleware("token");//列表
+
